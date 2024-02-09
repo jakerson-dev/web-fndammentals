d93f9df2940a7fe4956c7f522202e2f1aa28bf42
Jake <jakerson.dev@gmail.com>
2024-02-09 20:22:36 +0800

    update

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-function checkPrime(num) {
-    let isPrime = true;
-    
-    for (let index = 2; index <= num/2; index++) {
-        if ( num % index == 0){
-            isPrime = false;
-            break;
-        }
-    } 
-
-    return isPrime;
-}
-console.log(`Prime: ${checkPrime(2)}`)
+console.log(`Hello World`);
